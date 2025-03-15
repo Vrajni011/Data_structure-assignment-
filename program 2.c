928d45d47301d3d9073572931c45718016bf44ef
Vrajni011 <136429528+Vrajni011@users.noreply.github.com>
2025-03-15 10:18:50 +0300

    Update program 2.c

--- program 2.c
+++ program 2.c
@@ -3,18 +3,17 @@
 int main() {
     int N, X;
 
-    // Input number of domestic animals
+   
     printf("Enter the number of domestic animals: ");
     scanf("%d", &N);
 
-    // Input number of wild animals
     printf("Enter the number of wild animals: ");
     scanf("%d", &X);
 
-    // Calculate total number of animals
+    
     int totalAnimals = N + X;
 
-    // Display total animals
+   
     printf("Total number of animals: %d\n", totalAnimals);
 
     return 0;
